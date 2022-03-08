07f0db942d0fb35eb4af5785275b5e0d2dc55de9
Tykok <treportelie12@gmail.com>
2022-03-08 13:03:06 +0100

    Add `àpplication.properties` in .gitignore and add class for webservice

---
+++ src/main/java/com/example/cv/webservice/AllPersonneWebService.java
@@ -0,0 +1,4 @@
+package com.example.cv.webservice;
+
+public class AllPersonneWebService {
+}
--- src/main/java/com/example/cv/webservice/PersonneWebService.java
+++ src/main/java/com/example/cv/webservice/PersonneWebService.java
@@ -2,6 +2,7 @@ package com.example.cv.webservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,22 +15,27 @@ public class PersonneWebService {
         System.out.println("URL : personne/test \nMethods : test()");
         return "Méthode test de personne";
     }
-
-    @GetMapping("id")
-    public String getUser(){
+    // http://localhost:8080/personne?id=:id
+    @GetMapping("")
+    public Integer getUser(@RequestParam Integer id){
+        System.out.println("URL : personne?id=" + id + "\nMethods : test()");
         // Return the user with the id given in the url
-        return "ok";
+        return id;
     }
 
+    // http://localhost:8080/personne/skills?id=:id
     @GetMapping("/skills")
-    public String getSkills(){
+    public Integer getSkills(@RequestParam Integer id){
+        System.out.println("URL : personne/skills?id=" + id + "\nMethods : test()");
         // This function return the skill of a people according to the id given in url
-        return "ok";
+        return id;
     }
 
-    @GetMapping("competences")
-    public String getCompetences(){
+    // http://localhost:8080/personne/competences?id=:id
+    @GetMapping("/competences")
+    public Integer getCompetences(@RequestParam Integer id){
+        System.out.println("URL : personne/competences?id=" + id + "\nMethods : test()");
         // This function return the competences of a people according to the id given in url
-        return "ok";
+        return id;
     }
 }
