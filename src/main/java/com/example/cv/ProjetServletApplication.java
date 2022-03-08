7b9f212ec826f80fcf8294d7310a3cb92fcb10ed
Bastien <bastien.maraval@outlook.fr>
2022-03-08 10:25:24 +0100

    Init project-servlet
    Signed-off-by: Bastien <bastien.maraval@outlook.fr>

---
+++ src/main/java/com/example/cv/ProjetServletApplication.java
@@ -0,0 +1,13 @@
+package com.example.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetServletApplication.class, args);
+    }
+
+}
